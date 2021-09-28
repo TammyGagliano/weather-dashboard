94be7f45b08f40d7c9cac38b9ba74db8b2df5ecb
Tammy Gagliano <tracsd@gmail.com>
2021-09-27 21:33:03 -0500

    Update readme file. Update Index file. Create JS variables and add comments for code to be placed.

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
 # weather-dashboard
 
-This is a simple web-app that allows you to search for a city and get the current weather and 5 day forecast. 
+This is a simple web-app that allows you to search for a city and get the current weather and 5 day forecast.
+
+When someone searches for a city, the current and future conditions for that city will be presented and then that city will added to the search history.
+
+When viewing current weather conditions for the city, the following is shown:
+
+1. City name
+2. Date
+3. Temperature
+4. Humidity
+5. Wind speed
+6. UV index
+
+When viewing the future weather conditions for the city, a 5-day forecast will be shown with the following information: 
+
+1. Date
+2. Temperature
+3. Humidity
+
+When a city in the search history is clicked, the current and future conditions for that city is presented again. 
+
+When the weather dashboard is opened, the last searched city forecast is presented.
--- script.js
+++ script.js
@@ -1,6 +1,38 @@
-var APIKey = "eceb814eda4b8860bda912a28ff8535f";
+// Variables 
+var ___ = document.getElementById("today-weather");
+var ___ = document.getElementById("city-name");
+var ___ = document.getElementById("picture");
+var ___ = document.getElementById("temperature");
+var ___ = document.getElementById("humidity");
+var ___ = document.getElementById("wind-speed");
+var ___ = document.getElementById("UV-index");
+var ___ = document.getElementById("fiveday-header");
+
+
 var city;
 
+// Assigning a unique API to a variable
+var APIKey = "eceb814eda4b8860bda912a28ff8535f";
 
+// Execute a current weather get request from open weather api
+function getWeather(cityName) {
+
+}
 // api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
-// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
+
+
+// Parse response to display current weather
+
+
+// Get UV Index
+
+// When UV Index is good, shows green, when okay shows yellow, when bad shows red
+
+// Get 5 day forecast for city
+// Parse response to display forecast for next 5 days
+
+// Get history from local storage 
+
+//Clear history button
+
