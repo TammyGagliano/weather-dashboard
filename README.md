6fd13b9e2bbf8dcb6ded5bcf94bd23eaafa7cea9
Tammy Gagliano <tracsd@gmail.com>
2021-09-27 20:55:29 -0500

    update index.html file to add framework

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # weather-dashboard
+
+This is a simple web-app that allows you to search for a city and get the current weather and 5 day forecast. 
---
+++ script.js
@@ -0,0 +1,6 @@
+var APIKey = "eceb814eda4b8860bda912a28ff8535f";
+var city;
+
+
+// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+// var queryURL = "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=" + APIKey;
