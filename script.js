cbcb410ba4f2cf32b7984cc8d60c52685682f532
Tammy Gagliano <tracsd@gmail.com>
2021-10-03 11:09:55 -0500

    Update index and css file

--- script.js
+++ script.js
@@ -7,9 +7,15 @@ var temp = document.getElementById("temperature");
 var humid = document.getElementById("humidity");
 var wind = document.getElementById("wind-speed");
 var UV = document.getElementById("UV-index");
-
+var clearHistory = document.getElementById('clear-history');
 //var ___ = document.getElementById("picture");
 //var ___ = document.getElementById("fiveday-header");
+
+// Other variables 
+var lon;
+var lat;
+var uvIndex;
+var icon;
 
 // Assigning a unique API to a variable
 var APIKey = "eceb814eda4b8860bda912a28ff8535f";
@@ -20,14 +26,51 @@ var displayName = "";
 
 // display search results in container 
 
+
+
 function displayCurrentWeather (current) {
     console.log('Display current weather', current);
-    // temp, humidity, windspeed, UV
-    cityName.innerText = 'displayName' + ' ' + new Date(current.dt*1000);
     
-}
+    cityName.innerText = 'displayName' + ' ' + new Date(current.dt*1000);
+    // NEED TO USE THIS FOR DATE // var date = moment().format(" MM/DD/YYYY");
+    
+    // temp, humidity, windspeed, UV
+    //need to add an icon
+
+    // should I create an object to hold data?
+    var currentWeatherInfo = {
+      date: ,
+      temp: , 
+      humidity: , 
+      wind_speed: , 
+      UV: , 
+    }
+
+};
+
+
   
-// Display forecast 
+// Display 5-day forecast 
+
+function fiveDayForecast () {
+  var fiveDay = "https://api.openweathermap.org/data/2.5/forecast?id=" + fiveDay + "&appid=" + APIKey;"
+  .then (function (fiveDayResponse) {
+    {
+      return resp.json(fiveDayResponse);
+    })
+    .then(function (data) {
+
+  
+}
+
+// Save history from local storage 
+
+var saveCity = function(cityName) {
+  
+
+  localStorage.setItem("Grabbing city name and storing", JSON.stringify(cityName));
+}
+
 
 
 //EVENT LISTENERS
@@ -62,7 +105,7 @@ searchBox.addEventListener("click", function () {
       })
       .then(function(oneCallData) {
           console.log('Getting One Call data', oneCallData);
-          displayCurrentWeather(oneCallData.current);  // calling function 
+          displayCurrentWeather(oneCallData.current);  // calling function current and daily forecast
       })
     })
 
@@ -71,8 +114,15 @@ searchBox.addEventListener("click", function () {
     });
 });
 
+// Clear History
 
-// api.openweathermap.org/data/2.5/weather?q={city name}&appid={API key}
+ clearHistory.addEventListener("click", function () {
+  localStorage.clear();
+  
+})
+
+
+
 
 // Get UV Index
 // When UV Index is good, shows green, when okay shows yellow, when bad shows red
@@ -80,3 +130,16 @@ searchBox.addEventListener("click", function () {
 // Parse response to display forecast for next 5 days
 // Get history from local storage
 //Clear history button
+
+
+// GIVEN a weather dashboard with form inputs
+// WHEN I search for a city
+// THEN I am presented with current and future conditions for that city and that city is added to the search history
+// WHEN I view current weather conditions for that city
+// THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, the wind speed, and the UV index
+// WHEN I view the UV index
+// THEN I am presented with a color that indicates whether the conditions are favorable, moderate, or severe
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
+// WHEN I click on a city in the search history
+// THEN I am again presented with current and future conditions for that city
