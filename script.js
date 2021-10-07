40a6efe056fd0c3b4c0296bcdaba72b5f08ec3fc
Tammy Gagliano <tracsd@gmail.com>
2021-10-07 17:11:09 -0500

    City showing up in current area and stores in local storage

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ var weather = document.getElementById("today-weather");
 var searchBox = document.getElementById("search-button");
 var enterCity = document.getElementById("enter-city");
 var cityName = document.getElementById("city-name");
+var savedSearches = document.getElementById("saved-searches");
 var temp = document.getElementById("temperature");
 var humid = document.getElementById("humidity");
 var wind = document.getElementById("wind-speed");
@@ -20,7 +21,16 @@ var displayName = "";
 // NEED TO USE THIS FOR DATE // var date = moment().format(" MM/DD/YYYY");
 
 function displayCurrentWeather(current) {
+  //event.preventDefault();
   console.log("Display current weather", current);
+
+  var userCity = enterCity.value.trim();
+    var savedCity = document.createElement("button");
+    savedCity.className = "list-group-item";
+    savedCity.innerHTML = enterCity.value;
+    savedSearches.appendChild(savedCity);
+
+    cityName.innerHTML = userCity + " " + currentDate;
   // document.getElementById("city-name").innerHTML =
   //   +" " + new Date(current.dt * 1000).toLocaleString();
 
@@ -39,12 +49,11 @@ function displayCurrentWeather(current) {
     .getElementById("weather-icon")
     .setAttribute(
       "src",
-      "http://openweathermap.org/img/wn/" + current.weather[0].icon + "@2x.png"
+      "http://openweathermap.org/img/wn/" + current.weather[0].icon + "@2x.png",
+
     );
   //console.log(current.uvi);
-
   // When UV Index is good, shows green, when ok shows yellow, when bad shows red
-
   var uvIndex = document.querySelector("#UV-index");
   var currentUvi = current["uvi"];
   uvIndex.textContent = currentUvi;
@@ -68,6 +77,7 @@ function displayCurrentWeather(current) {
 // Display 5-day forecast
 
 function fiveDayForecast(daily) {
+
   for (i = 0; i < 5; i++) {
     document.getElementById("day" + i).children[0].innerHTML =
       daily[i].dt * 1000;
@@ -81,40 +91,35 @@ function fiveDayForecast(daily) {
       daily[i].temp.day + " &#8457;";
     document.getElementById("day" + i).children[3].innerHTML =
       daily[i].humidity + " &#37;";
-
     // console.log(daily[i].temp.day);
   }
 }
 
-var buildSearchHistory = function () {
-  // get search history from local storage
-  var searchHistory = JSON.parse(localStorage.getItem("searchHistory"));
-  if (searchHistory == null) {
-    // if the search history local variable does not exist then generate the left column with common locations
-    searchHistory = [];
-    localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
-  }
-  var groupContainer = $(".list-group");
-  groupContainer.html("");
-  for (i in searchHistory) {
-    // generate a list group item for each city in search history
-    var buttonEL = $("<button>")
-      .addClass("list-group-item list-group-item-action")
-      .attr("id", "enter-city-list")
-      .attr("type", "button")
-      .text(searchHistory[i]);
-    groupContainer.append(buttonEl);
-  }
-};
+var cities = [];
 
-// Save history from local storage
+if (localStorage.getItem("userCity")) {
+  cities = localStorage.getItem("userCity");
+  var cityHistory = [];
+  cityHistory = cities.split(",");
+  cities = cityHistory
 
-var saveCity = function () {
-  var searchHistory = document.getElementById("enter-city").value;
-  JSON.parse(window.localStorage.getItem(searchHistory));
-  //push
-  localStorage.setItem("searchHistory", JSON.stringify(searchHistory)); //convert to string
-};
+  for (var i = 0; i < cityHistory.length; i++) {
+      var appendCity = document.createElement("button");
+      appendCity.classList = "list-group-item";
+      appendCity.innerHTML = cityHistory[i];
+      savedSearches.append(appendCity); 
+  }
+} 
+
+function addToArray () {
+var userCity = enterCity.value.trim()
+var addCityArray = cities
+   addCityArray.push(userCity)
+   //console.log("add to city array", addCityArray);
+   //console.log("cities array", cities);
+   localStorage.setItem("userCity", addCityArray);
+}
+
 
 //EVENT LISTENERS
 //set up event listener on the search button when enter a city
@@ -151,7 +156,7 @@ searchBox.addEventListener("click", function () {
           console.log("Getting One Call data", oneCallData);
           displayCurrentWeather(oneCallData.current);
           fiveDayForecast(oneCallData.daily); // calling function current and daily forecast
-          saveCity();
+          addToArray();
         });
     })
 
@@ -162,6 +167,4 @@ searchBox.addEventListener("click", function () {
 
 // Clear History
 
-clearHistory.addEventListener("click", function () {
-  localStorage.clear();
-});
+searchBox.addEventListener("click", addToArray);
